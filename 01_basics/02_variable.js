d88ec982314cea5f3db6c7456c7f209980116fe5
Wish-Code-Sh <gm2047987@gmail.com>
2025-02-21 08:35:08 +0000

    commit

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,22 @@
+const userId = 123456
+let userEmail = "gm2047987@gmail.com"
+var userPassword = "wish@123"
+userCity = "Lalitpur"
+let userState 
+
+
+// cont are not changeable 
+//userId to 123456 will not chanage
+//userId = 123456
+/*
+Prefer not use var
+because of issue in block scope and functional scope
+*/
+
+userEmail = "Nepal.123@gmail.com"
+userPassword = "456789"
+userCity = "Kathmandu"
+
+
+console.table([userId,userEmail, userPassword, userCity, userState])
+
---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+// number => 2 to power 53
+// bigint
+// string => "dgfudsgf"
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol  => unique
+
+
+// object
+
+let age = 18
+console.log(typeof "Gaurav");// string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefine
