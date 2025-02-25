da4bfd90991e8eb775d4042fed1a16074ac3670d
Wish-Code-Sh <gm2047987@gmail.com>
2025-02-25 07:40:37 +0000

    datatypes summary

---
+++ 01_basics/conversion.js
@@ -0,0 +1,19 @@
+//let score = "33abc"
+//let score = null
+let score = undefined
+//let score = false
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number(score);
+console.log(ValueInNumber);
+console.log(typeof ValueInNumber);
+let IsLoggedIn = "Gaurav"
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+let score_1 = 65
+let scoreInString = String(score_1);
+console.log(scoreInString);
+console.log(typeof scoreInString);
---
+++ 01_basics/operation.js
@@ -0,0 +1,35 @@
+/*let value = 3
+let negValue = -value
+let gauravIn = String(negValue)
+console.log(typeof gauravIn);
+console.log(gauravIn);
+console.log(negValue); 
+console.log(typeof negValue);   
+let str1 = "Gaurav"
+let str2 = "Mishra"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2);
+console.log("1" + 2 +3 );
+console.log(1 + 2 + "3");*/
+console.log(3 + 4 * 5 % 3); 
+console.log((3 + 4) * (5 % 3)); 
+console.log((5 % 3));
+console.log(5 / 3);
+console.log(+true)
+console.log(+"")
+
+console.log(-true)
+console.log(-"")
+
+let num1 , num2 , num3
+num1 = num2 = num3 = (2+2)
+console.table([num1 , num2 ,num3])
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+let gameCounter2 = 100
+++gameCounter2
+console.log(gameCounter2)
