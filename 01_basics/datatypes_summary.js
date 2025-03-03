1a71b33f7463be3222b3eea38731f2ae3529693d
Wish-Code-Sh <gm2047987@gmail.com>
2025-03-03 06:28:34 +0000

    string

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -35,3 +35,31 @@ const myFunction = function(){
 console.log(typeof bigNumber);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive)  Heap (Non Primitive)
+
+let myInstaName = "Wish_Shekahr"
+
+let myFbName = myInstaName
+myFbName = "Gaurav"
+
+console.log(myFbName)
+console.log(myInstaName)
+
+let userOne = {
+       userEmail: "Wish@gmail.com",
+       age : 25,
+       userId: "Wish@"
+}
+
+ let userTwo = userOne
+ userOne.userEmail = "gm2047987@gmail.com"
+ userOne.age = 28
+ userOne.userFirstName = "Gaurav Shekhar"
+ userOne.userFavCar = "G-WAGON"
+ userOne.userLastName = 
+
+ console.table(userOne)
+ console.log(userTwo)
